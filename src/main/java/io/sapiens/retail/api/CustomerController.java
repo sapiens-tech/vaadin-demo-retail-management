f744b3ebf1515ba3f9475ceff825cafa91b8af93
Thai Tran <thaitcao@gmail.com>
2021-02-15 23:11:07 +0700

    add version for customer api

--- src/main/java/io/sapiens/retail/api/CustomerController.java
+++ src/main/java/io/sapiens/retail/api/CustomerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
 
   private final CustomerService customerService;
