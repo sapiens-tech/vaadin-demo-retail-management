a8511609fe866907f8b3a3963738cefe41ab4a08
Thai Tran <jason.tran@octagonsolution.com>
2021-03-07 08:31:28 +0700

    add product size, product size view

--- src/main/java/io/sapiens/retail/ui/views/inventories/ProductSizeView.java
+++ src/main/java/io/sapiens/retail/ui/views/inventories/ProductSizeView.java
@@ -3,7 +3,6 @@ package io.sapiens.retail.ui.views.inventories;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import io.sapiens.awesome.ui.views.CrudView;
-import io.sapiens.awesome.ui.views.SplitViewFrame;
 import io.sapiens.retail.ui.BaseLayout;
 import io.sapiens.retail.ui.models.ProductSize;
 
@@ -11,25 +10,30 @@ import java.util.List;
 
 @PageTitle("ProductSizes")
 @Route(value = "product-sizes", layout = BaseLayout.class)
-public class ProductSizeView extends SplitViewFrame {
+public class ProductSizeView
+    extends CrudView<ProductSize.List, ProductSize.Edit, ProductSize.Mapper> {
 
-//  @Override
-//  public void onInit() {}
-//
-//  @Override
-//  public void onSave(ProductSize productSize) {}
-//
-//  @Override
-//  public void onDelete(ProductSize productSize) {}
-//
-//  @Override
-//  public void onCancel() {}
-//
-//  @Override
-//  public List<String> onValidate(ProductSize entity) {
-//    return null;
-//  }
-//
-//  @Override
-//  public void filter() {}
+  public ProductSizeView() {
+    super(ProductSize.List.class, ProductSize.Edit.class, new ProductSize.Mapper());
+  }
+
+  @Override
+  public void onInit() {}
+
+  @Override
+  public void onSave(ProductSize.Edit entity) {}
+
+  @Override
+  public void onDelete(ProductSize.Edit entity) {}
+
+  @Override
+  public void onCancel() {}
+
+  @Override
+  public void filter() {}
+
+  @Override
+  public List<String> onValidate(ProductSize.Edit entity) {
+    return null;
+  }
 }
