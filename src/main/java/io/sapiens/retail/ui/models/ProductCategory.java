bd18b7a5caa1946af6565d1a314099d53c79938c
Thai Tran <jason.tran@octagonsolution.com>
2021-03-07 08:22:14 +0700

    add ack product category view

--- src/main/java/io/sapiens/retail/ui/models/ProductCategory.java
+++ src/main/java/io/sapiens/retail/ui/models/ProductCategory.java
@@ -3,21 +3,49 @@ package io.sapiens.retail.ui.models;
 import io.sapiens.awesome.ui.annotations.FormField;
 import io.sapiens.awesome.ui.annotations.GridColumn;
 import io.sapiens.awesome.ui.enums.FormFieldType;
+import io.sapiens.awesome.ui.views.CrudMapper;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.BeanUtils;
 
 @Getter
 @Setter
 public class ProductCategory {
-  @GridColumn(header = "Code")
-  @FormField(type = FormFieldType.TextField, label = "Code")
-  private String code;
 
-  @GridColumn(header = "Name")
-  @FormField(type = FormFieldType.TextField, label = "Name")
-  private String name;
+  @Getter
+  @Setter
+  public static class List {
+    @GridColumn(header = "Code")
+    @FormField(type = FormFieldType.TextField, label = "Code")
+    private String code;
 
-  @GridColumn(header = "Description", flexGrow = 1)
-  @FormField(type = FormFieldType.TextField, label = "Description")
-  private String description;
+    @GridColumn(header = "Name")
+    @FormField(type = FormFieldType.TextField, label = "Name")
+    private String name;
+
+    @GridColumn(header = "Description", flexGrow = 1)
+    @FormField(type = FormFieldType.TextField, label = "Description")
+    private String description;
+  }
+
+  @Getter
+  @Setter
+  public static class Edit {
+    @FormField(type = FormFieldType.TextField, label = "Code")
+    private String code;
+
+    @FormField(type = FormFieldType.TextField, label = "Name")
+    private String name;
+
+    @FormField(type = FormFieldType.TextField, label = "Description")
+    private String description;
+  }
+
+  public static class Mapper extends CrudMapper<List, Edit> {
+    public Edit fromListToEdit(List l) {
+      Edit e = new Edit();
+      BeanUtils.copyProperties(l, e);
+      return e;
+    }
+  }
 }
--- src/main/java/io/sapiens/retail/ui/views/inventories/ProductCategoryView.java
+++ src/main/java/io/sapiens/retail/ui/views/inventories/ProductCategoryView.java
@@ -2,9 +2,38 @@ package io.sapiens.retail.ui.views.inventories;
 
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import io.sapiens.awesome.ui.views.SplitViewFrame;
+import io.sapiens.awesome.ui.views.CrudView;
 import io.sapiens.retail.ui.BaseLayout;
+import io.sapiens.retail.ui.models.ProductCategory;
+
+import java.util.List;
 
 @Route(value = "product-categories", layout = BaseLayout.class)
 @PageTitle("Product Categories")
-public class ProductCategoryView extends SplitViewFrame {}
+public class ProductCategoryView
+    extends CrudView<ProductCategory.List, ProductCategory.Edit, ProductCategory.Mapper> {
+
+  public ProductCategoryView() {
+    super(ProductCategory.List.class, ProductCategory.Edit.class, new ProductCategory.Mapper());
+  }
+
+  @Override
+  public void onInit() {}
+
+  @Override
+  public void onSave(ProductCategory.Edit entity) {}
+
+  @Override
+  public void onDelete(ProductCategory.Edit entity) {}
+
+  @Override
+  public void onCancel() {}
+
+  @Override
+  public void filter() {}
+
+  @Override
+  public List<String> onValidate(ProductCategory.Edit entity) {
+    return null;
+  }
+}
