929819ae48cddc772b821f6dafa994fc423582fd
Thai Tran <jason.tran@octagonsolution.com>
2021-03-07 08:27:02 +0700

    add product view

--- src/main/java/io/sapiens/retail/ui/models/Product.java
+++ src/main/java/io/sapiens/retail/ui/models/Product.java
@@ -3,26 +3,49 @@ package io.sapiens.retail.ui.models;
 import io.sapiens.awesome.ui.annotations.FormField;
 import io.sapiens.awesome.ui.annotations.GridColumn;
 import io.sapiens.awesome.ui.enums.FormFieldType;
+import io.sapiens.awesome.ui.views.CrudMapper;
+import io.sapiens.awesome.ui.views.CrudView;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
 public class Product {
+  @Setter
+  @Getter
+  public static class List {
+    @GridColumn(header = "Product Sku")
+    @FormField(type = FormFieldType.TextField, label = "Product Sku")
+    private String productSku;
 
-  @GridColumn(header = "Product Sku")
-  @FormField(type = FormFieldType.TextField, label = "Product Sku")
-  private String productSku;
+    @GridColumn(header = "Name")
+    private String name;
 
-  @GridColumn(header = "Name")
-  @FormField(type = FormFieldType.TextField, label = "Name")
-  private String name;
+    @GridColumn(header = "Short Description")
+    private String shortDescription;
 
-  @GridColumn(header = "Short Description")
-  @FormField(type = FormFieldType.TextField, label = "Short Description")
-  private String shortDescription;
+    @GridColumn(header = "Long Description", flexGrow = 1)
+    private String longDescription;
+  }
 
-  @GridColumn(header = "Long Description", flexGrow = 1)
-  @FormField(type = FormFieldType.TextField, label = "Long Description")
-  private String longDesceription;
+  @Setter
+  @Getter
+  public static class Edit {
+    @FormField(type = FormFieldType.TextField, label = "Product Sku")
+    private String productSku;
+
+    @FormField(type = FormFieldType.TextField, label = "Name")
+    private String name;
+
+    @FormField(type = FormFieldType.TextField, label = "Short Description")
+    private String shortDescription;
+
+    @FormField(type = FormFieldType.TextField, label = "Long Description")
+    private String longDescription;
+  }
+
+  public static class Mapper extends CrudMapper<List, Edit> {
+    @Override
+    public Edit fromListToEdit(List l) {
+      return null;
+    }
+  }
 }
--- src/main/java/io/sapiens/retail/ui/views/inventories/ProductView.java
+++ src/main/java/io/sapiens/retail/ui/views/inventories/ProductView.java
@@ -2,9 +2,47 @@ package io.sapiens.retail.ui.views.inventories;
 
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import io.sapiens.awesome.ui.views.CrudView;
 import io.sapiens.awesome.ui.views.SplitViewFrame;
 import io.sapiens.retail.ui.BaseLayout;
+import io.sapiens.retail.ui.models.Product;
+
+import java.util.List;
 
 @PageTitle("Products")
 @Route(value = "products", layout = BaseLayout.class)
-public class ProductView extends SplitViewFrame {}
+public class ProductView extends CrudView<Product.List, Product.Edit, Product.Mapper> {
+    public ProductView() {
+        super(Product.List.class, Product.Edit.class, new Product.Mapper());
+    }
+
+    @Override
+    public void onInit() {
+
+    }
+
+    @Override
+    public void onSave(Product.Edit entity) {
+
+    }
+
+    @Override
+    public void onDelete(Product.Edit entity) {
+
+    }
+
+    @Override
+    public void onCancel() {
+
+    }
+
+    @Override
+    public void filter() {
+
+    }
+
+    @Override
+    public List<String> onValidate(Product.Edit entity) {
+        return null;
+    }
+}
